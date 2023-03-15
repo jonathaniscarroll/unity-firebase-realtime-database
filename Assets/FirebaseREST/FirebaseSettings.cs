90c07b6fd4ed02bd4e6bc78177a31f30e8e2b0e1
Jonathan Edward Carroll <jonathaniscarroll@gmail.com>
2023-03-15 14:32:17 -0300

    post Tinc

--- Assets/FirebaseREST/FirebaseSettings.cs
+++ Assets/FirebaseREST/FirebaseSettings.cs
@@ -3,7 +3,7 @@ namespace FirebaseREST
 {
     public class FirebaseSettings
     {
-        public const string DATABASE_URL = "https://there-is-no-centre-default-rtdb.firebaseio.com/";
-        public const string WEB_API = "AIzaSyChvxITwRXT9iiqWKUVMmmWQaWEgNQrRNs";
+        public const string DATABASE_URL = "https://cruise-35148.firebaseio.com/";
+        public const string WEB_API = "AIzaSyA1YLHuz-ahdplN5lVz7GpF56Bcvmn_wNI";
     }
 }
